3356ad8d8dfca6dea221126691c739196fdd9857
AlinaPerepechenko <alina.desiresv11@gmail.com>
2026-04-07 11:26:01 +0300

    Update OpenAccountPage.js

--- src/pages/manager/OpenAccountPage.js
+++ src/pages/manager/OpenAccountPage.js
@@ -1,13 +1,32 @@
 import { expect } from '@playwright/test';
-
-export class AddCustomerPage {
+ 
+export class OpenAccountPage {
   constructor(page) {
     this.page = page;
+    this.customerDropdown = page.getByTestId('userSelect');
+    this.currencyDropdown = page.getByTestId('currency');
+    this.processButton = page.getByRole('button', { name: 'Process' });
   }
-
+ 
   async open() {
     await this.page.goto(
       '/angularJs-protractor/BankingProject/#/manager/openAccount',
     );
+  }
+ 
+  async selectCustomer(customerName) {
+    await this.customerDropdown.selectOption({ label: customerName });
+  }
+ 
+  async selectCurrency(currency) {
+    await this.currencyDropdown.selectOption(currency);
+  }
+ 
+  async clickProcessButton() {
+    await this.processButton.click();
+  }
+ 
+  async assertCurrencyDropdownHasValue(value) {
+    await expect(this.currencyDropdown).toHaveValue(value);
   }
 }
