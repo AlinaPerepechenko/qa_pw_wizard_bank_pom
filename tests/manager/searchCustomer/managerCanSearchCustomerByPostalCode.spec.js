f1bbf83b5b305f42ca8c5f5ae79d7469af27b862
AlinaPerepechenko <alina.desiresv11@gmail.com>
2026-04-07 11:29:57 +0300

    Update managerCanSearchCustomerByPostalCode.spec.js

--- tests/manager/searchCustomer/managerCanSearchCustomerByPostalCode.spec.js
+++ tests/manager/searchCustomer/managerCanSearchCustomerByPostalCode.spec.js
@@ -1,11 +1,14 @@
 import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
-
+import { AddCustomerPage } from '../../../src/pages/manager/AddCustomerPage';
+import { CustomersListPage } from '../../../src/pages/manager/CustomersListPage';
+ 
 let firstName;
 let lastName;
 let postalCode;
-
+ 
 test.beforeEach(async ({ page }) => {
+  const addCustomerPage = new AddCustomerPage(page);
   /* 
   Pre-conditons:
   1. Open Add Customer page.
@@ -17,6 +20,10 @@ test.beforeEach(async ({ page }) => {
   firstName = faker.person.firstName();
   lastName = faker.person.lastName();
   postalCode = faker.location.zipCode();
+ 
+  await addCustomerPage.open();
+  await addCustomerPage.addCustomer(firstName, lastName, postalCode);
+  await page.reload();
 });
 
 test('Assert manager can search customer by Postal Code', async ({ page }) => {
@@ -27,4 +34,13 @@ test('Assert manager can search customer by Postal Code', async ({ page }) => {
   3. Assert customer row is present in the table. 
   4. Assert no other rows is present in the table.
   */
+
+  const customersListPage = new CustomersListPage(page);
+ 
+  await customersListPage.open();
+  await customersListPage.fillSearchInput(postalCode);
+  await customersListPage.assertCustomerRowIsVisible(firstName, lastName);
+  await customersListPage.assertOnlyOneRowIsVisible();
+
 });
+
