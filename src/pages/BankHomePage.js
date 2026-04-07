a4c7372e08a191ce54919c48f3c365dc0ded04b8
AlinaPerepechenko <alina.desiresv11@gmail.com>
2026-04-07 11:26:24 +0300

    Update BankHomePage.js

--- src/pages/BankHomePage.js
+++ src/pages/BankHomePage.js
@@ -1,18 +1,25 @@
 import { expect } from '@playwright/test';
-
+ 
 export class BankHomePage {
   constructor(page) {
     this.page = page;
     this.customerLoginButton = page.getByRole('button', {
       name: 'Customer Login',
     });
+    this.bankManagerLoginButton = page.getByRole('button', {
+      name: 'Bank Manager Login',
+    });
   }
-
+ 
   async open() {
     await this.page.goto('/angularJs-protractor/BankingProject/#/login');
   }
-
+ 
   async clickCustomerLoginButton() {
     await this.customerLoginButton.click();
+  }
+ 
+  async clickBankManagerLoginButton() {
+    await this.bankManagerLoginButton.click();
   }
 }
