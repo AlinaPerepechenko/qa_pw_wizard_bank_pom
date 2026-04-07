bf5137378af591cee112962d8f13e4ff7858732d
AlinaPerepechenko <alina.desiresv11@gmail.com>
2026-04-07 11:28:15 +0300

    Update managerCanDeleteCustomer.spec.js

--- tests/manager/deleteCustomer/managerCanDeleteCustomer.spec.js
+++ tests/manager/deleteCustomer/managerCanDeleteCustomer.spec.js
@@ -1,7 +1,23 @@
 import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
-
+import { AddCustomerPage } from '../../../src/pages/manager/AddCustomerPage';
+import { CustomersListPage } from '../../../src/pages/manager/CustomersListPage';
+ 
+let firstName;
+let lastName;
+let postalCode;
+ 
 test.beforeEach(async ({ page }) => {
+  const addCustomerPage = new AddCustomerPage(page);
+ 
+  firstName = faker.person.firstName();
+  lastName = faker.person.lastName();
+  postalCode = faker.location.zipCode();
+ 
+  await addCustomerPage.open();
+  await addCustomerPage.addCustomer(firstName, lastName, postalCode);
+  await page.reload();
+});
   /* 
   Pre-conditons:
   1. Open Add Customer page.
@@ -21,4 +37,13 @@ test('Assert manager can delete customer', async ({ page }) => {
   4. Reload the page.
   5. Assert customer row is not present in the table. 
   */
+
+  const customersListPage = new CustomersListPage(page);
+ 
+  await customersListPage.open();
+  await customersListPage.clickDeleteForCustomer(firstName, lastName);
+  await customersListPage.assertCustomerRowIsHidden(firstName, lastName);
+ 
+  await page.reload();
+  await customersListPage.assertCustomerRowIsHidden(firstName, lastName);
 });
